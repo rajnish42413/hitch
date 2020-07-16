3c76afebeb246aedc00007acbfe082c86ac5b2dd
Rajnish Singh <rajnish42413@gmail.com>
2020-07-16 17:38:20 +0530

    frontend color setup

--- src/constants/general.tsx
+++ src/constants/general.tsx
@@ -1,1 +1,7 @@
-export const GITHUB_LINK = 'https://github.com/LucasBassetti/react-typescript-antd-boilerplate';
+export  const colors ={
+      'primary-color': '#60BD9B',
+      'white-color' : '#FFFFFF',
+      'black-color' :'#000000',
+      'border-color' : '#C1C1C1',
+      'mutted-color' :'#BDC3C7'
+  }
